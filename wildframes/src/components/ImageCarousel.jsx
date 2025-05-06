baa82a2fd78f6aef17ac0c6e4aeff30c59caea01
shashankgbhat <shashankgb2001@gmail.com>
2025-05-06 15:17:39 +0530

    new images

--- wildframes/src/components/ImageCarousel.jsx
+++ wildframes/src/components/ImageCarousel.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 
-const images = ["/images/img1.jpg", "/images/img2.jpg"];
+const images = [
+  "/images/img1.jpg",
+  "/images/img3.jpg",
+  "/images/img2.jpg",
+  "/images/img4.jpg",
+  "/images/img5.jpg",
+];
 
 const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
